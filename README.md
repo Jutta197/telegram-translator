d1de3f2ef7c0453bcfd352558272487198c1b074
Jutta197 <yangzigui99@gmail.com>
2024-08-23 15:10:30 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # telegram-translator
-Telegram messages display both original text and translation
+Use [MTProto API](https://core.telegram.org/api#tdlib--build-your-own-telegram) to log in to Telegram and display both the original text and its translation.
+
+
+## Python Environment Setup
+
+Code execution requires a [Python](https://www.python.org/downloads/windows/) environment:
+
+```sh
+python --version
+```
+
+## Clone the Repository
+
+```sh
+git clone git clone git@github.com:Jutta197/telegram-translator.git
+```
+
+## Navigate to the Project Directory
+
+```sh
+cd telegram-translator
+```
+
+## Install Dependencies
+
+```sh
+pip install -r packages.txt
+```
+
+## Configure API Credentials
+
+Visit [this](https://my.telegram.org/apps), log in, create an application, and then obtain **your API ID and API hash**. Replace these values in the **transdot3.py** file.
+```python
+api_id = 'api_id'
+api_hash = 'api_hash'
+```
+
+### Launch the Application
+
+```sh
+py transdot3.py
+```
---
+++ transdot3.py
@@ -0,0 +1,54 @@
+from telethon.sync import TelegramClient, events
+from translatepy import Translator, exceptions
+
+# https://my.telegram.org/apps
+api_id = 'api_id'
+api_hash = 'api_hash'
+
+
+# Create an instance of TelegramClient
+client = TelegramClient('transdot3', api_id, api_hash)
+client.start()
+
+# Initialize the translator
+translator = Translator()
+
+@client.on(events.NewMessage(outgoing=True))
+async def main(event):
+# Check for the `.tr` prefix to implement specific command handling functionality.
+#   if not event.message.message.startswith(".tr") and event.message.message.strip():  
+    if event.message.message.strip():
+        try:
+            # Translation
+            translated_text_ja = translator.translate(event.message.message, "ja").result
+            translated_text_de = translator.translate(event.message.message, "de").result
+
+            # Output the original text and the translations
+            # Or use the entities parameter https://core.telegram.org/api/entities
+            # f-string
+            combined_message = (
+                f"{event.message.message}\n\n"
+#                f"```"
+                f"「{translated_text_ja}」"
+                f"{translated_text_de}"
+#                f"```"
+
+            )
+
+            # Update the message content
+            await event.edit(combined_message)
+        except (exceptions.NoResult, exceptions.UnknownLanguage) as error:
+            await event.delete()
+            await client.send_message(
+                "me",
+                f"Error occurred while translating the message '{event.message.message}':\n{error}\n\nPlease try another language."
+            )
+        except Exception as e:
+            await client.send_message(
+                "me",
+                f"An unexpected error occurred: {e}"
+            )
+            
+
+# Run the client
+client.run_until_disconnected()
